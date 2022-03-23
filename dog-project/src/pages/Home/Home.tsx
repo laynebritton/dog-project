a982ac8fd050932fef447d88a35a540241878bc2
laynebritton <laynebritton1998@gmail.com>
2022-03-22 22:05:48 -0400

    Carousel styling

--- dog-project/src/pages/Home/Home.tsx
+++ dog-project/src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from "react";
-import { Container, Row, Col, Carousel } from "react-bootstrap";
+import { Container, Row, Col, Carousel, Card, Image } from "react-bootstrap";
 import { getRandomDogs } from "../../api/dog-api";
 import AnimalFrame from "../../components/AnimalFrame/AnimalFrame";
 import { Dog } from "../../interfaces/dog";
@@ -26,15 +26,20 @@ const Home: FC<HomeProps> = () => {
           {dogs &&
             dogs.map((dog) => (
               <Carousel.Item>
+                {/* <Image src={dog.image_url} fluid></Image> */}
                 <img className={styles.carousel_image} src={dog.image_url} />
               </Carousel.Item>
             ))}
         </Carousel>
-        <Row></Row>
         <Row>
           <Col xs="1" sm="2"></Col>
           <Col xs="10" sm="8">
-            
+            <Card>
+              <Card.Body>
+                <Card.Title>Watch an endless stream of dogs</Card.Title>
+                <Card.Subtitle>Infinitely scrolling pups</Card.Subtitle>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
       </Container>
