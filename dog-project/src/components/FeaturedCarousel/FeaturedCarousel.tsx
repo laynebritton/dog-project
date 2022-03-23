710d7ac03d1b02deaa1ead1e5c8be5682f04a57f
laynebritton <laynebritton1998@gmail.com>
2022-03-23 12:07:53 -0400

    Featured carousel moved to its own component

---
+++ dog-project/src/components/FeaturedCarousel/FeaturedCarousel.tsx
@@ -0,0 +1,32 @@
+import React, { FC, useEffect, useState } from "react";
+import { Carousel } from "react-bootstrap";
+import { getRandomDogs } from "../../api/dog-api";
+import { Dog } from "../../interfaces/dog";
+import styles from "./FeaturedCarousel.module.css";
+
+interface FeaturedCarouselProps {}
+
+const ACCORDION_DOG_COUNT = 4;
+
+const FeaturedCarousel: FC<FeaturedCarouselProps> = () => {
+  const [featuredDogs, setFeaturedDogs] = useState<Dog[]>();
+
+  useEffect(() => {
+    getRandomDogs(ACCORDION_DOG_COUNT).then((dogs) => {
+      setFeaturedDogs(dogs);
+    });
+  }, []);
+
+  return (
+    <Carousel variant="dark">
+      {featuredDogs &&
+        featuredDogs.map((dog) => (
+          <Carousel.Item>
+            <img className={styles.carousel_image} src={dog.image_url} />
+          </Carousel.Item>
+        ))}
+    </Carousel>
+  );
+};
+
+export default FeaturedCarousel;
--- dog-project/src/pages/Home/Home.tsx
+++ dog-project/src/pages/Home/Home.tsx
@@ -2,23 +2,14 @@ import React, { FC, useEffect, useState } from "react";
 import { Container, Row, Col, Carousel, Card, Image } from "react-bootstrap";
 import { getRandomDogs } from "../../api/dog-api";
 import AnimalFrame from "../../components/AnimalFrame/AnimalFrame";
+import FeaturedCarousel from "../../components/FeaturedCarousel/FeaturedCarousel";
 import InfiniteDogs from "../../components/InfiniteDogs/InfiniteDogs";
 import { Dog } from "../../interfaces/dog";
 import styles from "./Home.module.css";
 
 interface HomeProps {}
 
-const ACCORDION_DOG_COUNT = 4;
-
 const Home: FC<HomeProps> = () => {
-  const [featuredDogs, setFeaturedDogs] = useState<Dog[]>();
-
-  useEffect(() => {
-    getRandomDogs(ACCORDION_DOG_COUNT).then((dogs) => {
-      setFeaturedDogs(dogs);
-    });
-  }, []);
-
   return (
     <div className={styles.Home} data-testid="Home">
       <Container fluid className={styles.carousel_spacing}>
@@ -36,17 +27,7 @@ const Home: FC<HomeProps> = () => {
         <Row>
           <Col className="d-none d-sm-block" sm="2"></Col>
           <Col xs="12" sm="8">
-            <Carousel variant="dark">
-              {featuredDogs &&
-                featuredDogs.map((dog) => (
-                  <Carousel.Item>
-                    <img
-                      className={styles.carousel_image}
-                      src={dog.image_url}
-                    />
-                  </Carousel.Item>
-                ))}
-            </Carousel>
+            <FeaturedCarousel />
           </Col>
         </Row>
         <Row>
