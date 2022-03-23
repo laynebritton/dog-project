a3497e90fa0c8953c744729711fa8fb800cb578f
laynebritton <laynebritton1998@gmail.com>
2022-03-23 00:25:14 -0400

    Infinite Scrolling Dog Component works

---
+++ dog-project/src/components/InfiniteDogs/InfiniteDogs.tsx
@@ -0,0 +1,53 @@
+import React, { FC, useEffect, useState } from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { getRandomDogs } from "../../api/dog-api";
+import { Dog } from "../../interfaces/dog";
+import AnimalFrame from "../AnimalFrame/AnimalFrame";
+import styles from "./InfiniteDogs.module.css";
+
+interface InfiniteDogsProps {}
+const INFINITE_DOG_LOAD_COUNT = 49;
+
+const InfiniteDogs: FC<InfiniteDogsProps> = () => {
+  const [infiniteDogs, setInfiniteDogs] = useState<Dog[]>([]);
+
+  useEffect(() => {
+    getRandomDogs(INFINITE_DOG_LOAD_COUNT).then((dogs) => {
+      setInfiniteDogs(dogs);
+    });
+  }, []);
+
+  const fetch_additional_dogs = () => {
+    getRandomDogs(INFINITE_DOG_LOAD_COUNT).then((dogs) => {
+      const temp = infiniteDogs.concat(dogs);
+      console.log(temp);
+      setInfiniteDogs(temp);
+    });
+    console.log("fetching!");
+  };
+
+  return (
+    <>
+      <InfiniteScroll
+        next={fetch_additional_dogs}
+        hasMore={true}
+        loader={<h4>Loading ...</h4>}
+        dataLength={infiniteDogs.length}
+      >
+        <Container fluid>
+          <Row>
+            {infiniteDogs &&
+              infiniteDogs.map((dog) => (
+                <Col xs="12" sm="4">
+                  <AnimalFrame imageUrl={dog.image_url} />
+                </Col>
+              ))}
+          </Row>
+        </Container>
+      </InfiniteScroll>
+    </>
+  );
+};
+
+export default InfiniteDogs;
--- dog-project/src/pages/Home/Home.tsx
+++ dog-project/src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React, { FC, useEffect, useState } from "react";
 import { Container, Row, Col, Carousel, Card, Image } from "react-bootstrap";
 import { getRandomDogs } from "../../api/dog-api";
 import AnimalFrame from "../../components/AnimalFrame/AnimalFrame";
+import InfiniteDogs from "../../components/InfiniteDogs/InfiniteDogs";
 import { Dog } from "../../interfaces/dog";
 import styles from "./Home.module.css";
 
@@ -10,36 +11,51 @@ interface HomeProps {}
 const ACCORDION_DOG_COUNT = 4;
 
 const Home: FC<HomeProps> = () => {
-  const [dogs, setDogs] = useState<Dog[]>();
+  const [featuredDogs, setFeaturedDogs] = useState<Dog[]>();
 
   useEffect(() => {
     getRandomDogs(ACCORDION_DOG_COUNT).then((dogs) => {
-      setDogs(dogs);
-      console.log(dogs);
+      setFeaturedDogs(dogs);
     });
   }, []);
 
   return (
     <div className={styles.Home} data-testid="Home">
       <Container fluid className={styles.carousel_spacing}>
-        <Carousel variant="dark">
-          {dogs &&
-            dogs.map((dog) => (
-              <Carousel.Item>
-                {/* <Image src={dog.image_url} fluid></Image> */}
-                <img className={styles.carousel_image} src={dog.image_url} />
-              </Carousel.Item>
-            ))}
-        </Carousel>
         <Row>
           <Col xs="1" sm="2"></Col>
           <Col xs="10" sm="8">
             <Card>
               <Card.Body>
                 <Card.Title>Watch an endless stream of dogs</Card.Title>
-                <Card.Subtitle>Infinitely scrolling pups</Card.Subtitle>
+                <Card.Subtitle>Featured Dogs Below</Card.Subtitle>
               </Card.Body>
             </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="d-none d-sm-block" sm="2"></Col>
+          <Col xs="12" sm="8">
+            <Carousel variant="dark">
+              {featuredDogs &&
+                featuredDogs.map((dog) => (
+                  <Carousel.Item>
+                    <img
+                      className={styles.carousel_image}
+                      src={dog.image_url}
+                    />
+                  </Carousel.Item>
+                ))}
+            </Carousel>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="1" sm="2"></Col>
+          <Col xs="10" sm="8">
+            <h1>Keep scrolling to see more dogs</h1>
+            <Row>
+              <InfiniteDogs />
+            </Row>
           </Col>
         </Row>
       </Container>
