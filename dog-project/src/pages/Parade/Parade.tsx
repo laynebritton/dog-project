b1c194e834472341fee6ae7ba1159ed79c162ad8
laynebritton <laynebritton1998@gmail.com>
2022-04-11 21:00:28 -0400

    Refactored get next dog from stack to standalone function.

--- dog-project/src/pages/Parade/Parade.tsx
+++ dog-project/src/pages/Parade/Parade.tsx
@@ -9,6 +9,9 @@ import * as CONSTANTS from '../../util/Constants';
 
 interface ParadeProps {}
 
+const MAX_PARADE_DOGS_ON_SCREEN_AT_ONCE = 7;
+const MINIMUM_DOGS_NEEDED_TO_LOAD_MORE = 10;
+
 const Parade: FC<ParadeProps> = () => {
   const [paradeDogs, setParadeDogs] = useState<ParadeDog[]>([]);
   const [infiniteDogs, setInfiniteDogs] = useState<Dog[]>([]);
@@ -26,15 +29,21 @@ const Parade: FC<ParadeProps> = () => {
   };
 
   const createParadeDog = (dog: Dog): ParadeDog => {
-    const randomY = Math.random() * (height - 100);
+    const randomY = Math.random() * (height - 200);
     const paradeDog = {
       x: -width / 2,
       y: randomY,
-      speed: Math.random() * 2,
+      speed: Math.random() * 2.5,
       movementFunction: moveVerticallyAcrossTheScreen,
       dog: dog
     };
     return paradeDog;
+  };
+
+  const getNextDogFromStack = (): Dog | undefined => {
+    const dog = infiniteDogs.pop();
+    setInfiniteDogs(infiniteDogs);
+    return dog;
   };
 
   useEffect(() => {
@@ -45,12 +54,11 @@ const Parade: FC<ParadeProps> = () => {
 
   useEffect(() => {
     if (paradeDogs.length < 1) {
-      const dog = infiniteDogs.pop();
+      const dog = getNextDogFromStack();
       if (!dog) {
         return;
       }
-      setInfiniteDogs(infiniteDogs);
-      const tempArray: ParadeDog[] = paradeDogs.slice();
+      const tempArray = paradeDogs.slice();
       tempArray.push(createParadeDog(dog));
       setParadeDogs(tempArray);
     }
@@ -58,7 +66,7 @@ const Parade: FC<ParadeProps> = () => {
 
   useEffect(() => {
     const newIntervalId = window.setInterval(() => {
-      if (infiniteDogs.length <= 10) {
+      if (infiniteDogs.length <= MINIMUM_DOGS_NEEDED_TO_LOAD_MORE) {
         getRandomDogs(CONSTANTS.INFINITE_DOG_LOAD_COUNT).then((dogs) => {
           const temp = infiniteDogs.concat(dogs);
           setInfiniteDogs(temp);
@@ -69,9 +77,8 @@ const Parade: FC<ParadeProps> = () => {
       }
       const tempArray = paradeDogs.slice();
 
-      if (paradeDogs.length < 6) {
-        const dog = infiniteDogs.pop();
-        setInfiniteDogs(infiniteDogs);
+      if (paradeDogs.length < MAX_PARADE_DOGS_ON_SCREEN_AT_ONCE) {
+        const dog = getNextDogFromStack();
         if (!dog) {
           return;
         }
