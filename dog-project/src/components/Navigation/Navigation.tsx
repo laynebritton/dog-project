bac353888d3a0bdf406072b92ef37341337f597f
laynebritton <laynebritton1998@gmail.com>
2022-06-17 18:10:23 -0400

    Navbar uses same color as body

--- dog-project/src/components/Navigation/Navigation.tsx
+++ dog-project/src/components/Navigation/Navigation.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import * as ROUTES from '../../util/Routes';
+import styles from './Navigation.module.css';
 
 interface NavigationProps {}
 
 const Navigation: FC<NavigationProps> = () => (
-  <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
+  <Navbar sticky="top" variant="dark" expand="lg" className={styles.Navigation}>
     <Container>
       <Navbar.Brand href={ROUTES.HOME}>Infinite Dogs</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -13,18 +14,6 @@ const Navigation: FC<NavigationProps> = () => (
         <Nav className="me-auto">
           <Nav.Link href={ROUTES.HOME}>Home</Nav.Link>
           <Nav.Link href={ROUTES.PARADE}>Parade</Nav.Link>
-          {/* <NavDropdown title="Parade" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Start</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Custom Parade
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link href={ROUTES.ABOUT}>Our Mission</Nav.Link> */}
         </Nav>
       </Navbar.Collapse>
     </Container>
