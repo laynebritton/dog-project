ead5c9be869c52f4f19ef23347fc2bc429538470
laynebritton <laynebritton1998@gmail.com>
2022-06-17 20:27:56 -0400

    experimental hero load anim

--- dog-project/src/components/Hero/Hero.tsx
+++ dog-project/src/components/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { motion } from 'framer-motion';
 
 interface HeroProps {}
 
@@ -22,10 +23,24 @@ const Hero: FC<HeroProps> = () => (
           textAlign: 'center'
         }}
       >
-        <h1 style={{ fontSize: '10vh', color: '#FFF' }}>Infinite Dogs </h1>
+        <motion.div
+          initial={{ opacity: 0.8, marginTop: 80 }}
+          animate={{ opacity: 1, marginTop: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <h1 style={{ fontSize: '10vh', color: '#FFF' }}>Infinite Dogs </h1>
+        </motion.div>
         <div style={{ display: 'block', width: '100%', height: '0' }} />
-        <p style={{ fontSize: '2rem', color: '#FFF' }}>Scroll for more dogs.</p>
-        <div style={{ display: 'block', width: '100%', height: '0' }} />
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.15, duration: 0.6 }}
+        >
+          <p style={{ fontSize: '2rem', color: '#FFF' }}>
+            Scroll for more dogs.
+          </p>
+        </motion.div>
       </div>
     </div>
   </>
